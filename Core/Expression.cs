db29fa37d3506b161173813797536ce9e41b2dff
mathe-man <mathieumousset6@gmail.com>
2025-06-07 19:59:43 +0300

    Add expression abstract class and childs

---
+++ Core/Expression.cs
@@ -0,0 +1,41 @@
+namespace Core;
+
+public abstract class Expression
+{
+    public abstract float Evaluate();
+}
+
+public class LiteralExpression : Expression
+{
+    public float Value;
+    public LiteralExpression(float value) { Value = value; }
+    public override float Evaluate() => Value;
+}
+
+public class BinaryExpression : Expression
+{
+    public Expression Left;
+    public string Operator;
+    public Expression Right;
+    
+    public BinaryExpression(Expression left, string op, Expression right)
+    {
+        Left = left;
+        Operator = op;
+        Right = right;
+    }
+
+    public override float Evaluate()
+    {
+        float l = Left.Evaluate();
+        float r = Right.Evaluate();
+        return Operator switch
+        {
+            "+" => l + r,
+            "-" => l - r,
+            "*" => l * r,
+            "/" => l / r,
+            _ => throw new Exception("Unknown operator: " + Operator)
+        };
+    }
+}
